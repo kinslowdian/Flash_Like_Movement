e7a26097d47c7bc2c09537ad055d8ea2aae721f2
Simon Kinslow <skinslow@skinslow.local>
2014-11-14 08:24:17 +0000

    Rebound Hit Testing
    Still buggy need to force hit area better to a safer x & y position

--- _assets/_js/Main.js
+++ _assets/_js/Main.js
@@ -6,6 +6,7 @@ var loopList;
 
 var HIT_TEST;
 
+var basic;
 
 $(document).ready(function(){ init(); });
 
@@ -45,6 +46,9 @@ function init()
 	// MOVE EASING
 	control.fl.move = 4;
 	control.fl.easing = 0.1;
+	control.fl.moveSafe = 20;
+
+	control.signal = false;
 
 	control.dir = "";
 
@@ -53,12 +57,73 @@ function init()
 	loopList = new Array();
 	loopList.push(onEnterFrame_dirs);
 	loopList.push(onEnterFrame_hitTest);
+	// HACK
+	// loopList.push(logic);
+	// HACK
 	loopList.push(onEnterFrame_move);
 
 	window.requestAnimationFrame(onEnterFrame);
 
+	createBasic();
+
 	move_init(true);
 }
+
+function createBasic()
+{
+	var _wall;
+
+	basic = {};
+
+	basic.list = new Array();
+
+	_wall = new wall("border-t", 0, -40, 320, 40);
+	basic.list.push(_wall);
+
+	_wall = new wall("border-b", 0, 500, 320, 40);
+	basic.list.push(_wall);
+
+	_wall = new wall("border-l", -40, 0, 40, 2000);
+	basic.list.push(_wall);
+
+	_wall = new wall("border-r", 320, 0, 40, 2000);
+	basic.list.push(_wall);
+
+	_wall = new wall("border-m", 0, 160, 120, 120);
+	basic.list.push(_wall);
+}
+
+var wall = function(_c, _x, _y, _w, _h)
+{
+	this.c = _c;
+	this.x = _x;
+	this.y = _y;
+	this.w = _w;
+	this.h = _h;
+}
+
+function logic()
+{
+	for(var i in basic.list)
+	{
+		// if(control.fl.sx < (basic.list[i].x + basic.list[i].w))
+		// {
+		// 	trace(basic.list[i]);
+		// }
+
+		// trace(basic.list[i].x + basic.list[i].w);
+
+
+		// if(control.fl.sx < (basic.list[i].x + basic.list[i].w))
+		// {
+		// 	if(control.fl.sy >= basic.list[i].y && control.fl.sy < basic.list[i].h)
+		// 	{
+		// 		trace(basic.list[i]);
+		// 	}
+		// }
+	}
+}
+
 
 function hitTest_init()
 {
@@ -88,6 +153,8 @@ function move_init(run)
 
 function move_event(event)
 {
+	var tempSignal = "";
+
 	if(event.type === "keyup")
 	{
 		control.dir = "STILL";
@@ -103,7 +170,8 @@ function move_event(event)
 			{
 				// LEFT
 
-				control.dir = "LEFT";
+				//	control.dir = "LEFT";
+				tempSignal = "LEFT";
 
 				break;
 			}
@@ -112,7 +180,8 @@ function move_event(event)
 			{
 				// UP
 
-				control.dir = "UP";
+				//	control.dir = "UP";
+				tempSignal = "UP";
 
 				break;
 			}
@@ -121,7 +190,8 @@ function move_event(event)
 			{
 				// RIGHT
 
-				control.dir = "RIGHT";
+				// control.dir = "RIGHT";
+				tempSignal = "RIGHT";
 
 				break;
 			}
@@ -130,7 +200,8 @@ function move_event(event)
 			{
 				// DOWN
 
-				control.dir = "DOWN";
+				// control.dir = "DOWN";
+				tempSignal = "DOWN";
 
 				break;
 			}
@@ -140,6 +211,13 @@ function move_event(event)
 				control.dir = "STILL";
 			}
 		}
+
+		if(control.signal)
+		{
+			control.dir = tempSignal;
+		}
+
+		// onEnterFrame_dirs();
 	}
 }
 
@@ -157,25 +235,79 @@ function onEnterFrame_dirs()
 {
 	var css;
 
+	var finalMoveX = 0;
+	var finalMoveY = 0;
+
 	if(control.dir === "UP")
 	{
-		control.fl.sy -= control.fl.move;
+		if(HIT_TEST.hit_edge)
+		{
+			control.dir = "DOWN";
+
+			finalMoveY = (control.fl.move * 1);
+		}
+
+		else
+		{
+			finalMoveY = -control.fl.move;
+		}
+
+		// control.fl.sy -= control.fl.move;
 	}
 
 	else if(control.dir === "DOWN")
 	{
-		control.fl.sy += control.fl.move;
+		if(HIT_TEST.hit_edge)
+		{
+			control.dir = "UP";
+
+			finalMoveY = -(control.fl.move * 1);
+		}
+
+		else
+		{
+			finalMoveY = control.fl.move;
+		}
+
+		// control.fl.sy += control.fl.move;
 	}
 
 	if(control.dir === "LEFT")
 	{
-		control.fl.sx -= control.fl.move;
+		if(HIT_TEST.hit_edge)
+		{
+			control.dir = "RIGHT";
+
+			finalMoveX = (control.fl.move * 1);
+		}
+
+		else
+		{
+			finalMoveX = -control.fl.move;
+		}
+
+		// control.fl.sx -= control.fl.move;
 	}
 
 	else if(control.dir === "RIGHT")
 	{
-		control.fl.sx += control.fl.move;
+		if(HIT_TEST.hit_edge)
+		{
+			control.dir = "LEFT";
+
+			finalMoveX = -(control.fl.move * 1);
+		}
+
+		else
+		{
+			finalMoveX = control.fl.move;
+		}
+
+		// control.fl.sx += control.fl.move;
 	}
+
+	control.fl.sx += finalMoveX;
+	control.fl.sy += finalMoveY;
 
 	css = {
 					"-webkit-transform"	: "translate(" + control.fl.sx + "px, " + control.fl.sy + "px)",
@@ -194,12 +326,16 @@ function onEnterFrame_hitTest()
 		if($(HIT_TEST.hits[0]).attr("data-npc") === "edge")
 		{
 			HIT_TEST.hit_edge = true;
+
+			control.signal = false;
 		}
 	}
 
 	else
 	{
 		HIT_TEST.hit_edge = false;
+
+		control.signal = true;
 	}
 
 	$(".status p").html(HIT_TEST.hit_edge.toString());
@@ -265,7 +401,7 @@ function onEnterFrame_move()
 
 	else
 	{
-		HIT_TEST.hit_edge = false;
+		// HIT_TEST.hit_edge = false;
 	}
 }
 
